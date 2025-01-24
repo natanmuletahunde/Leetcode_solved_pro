04fe28122e7ee1b0068810e79449bf596f2d0cf2
pivnati <natanmuleta911@gmail.com>
2025-01-24 14:26:26 -0700

    optimize the unique_list problem

--- linked_list/unique_list.py
+++ linked_list/unique_list.py
@@ -9,15 +9,15 @@ class Solution(object):
         if not head or left == right:
             return head
         
-        dummy = ListNode(0)  # Create a dummy node to simplify edge cases
+        dummy = ListNode(0)  
         dummy.next = head
         prev = dummy
         
-        # Move prev to the node before the left position
+        
         for _ in range(left - 1):
             prev = prev.next
         
-        # Reverse the sublist from left to right
+       
         current = prev.next
         next_node = None
         for _ in range(right - left):
