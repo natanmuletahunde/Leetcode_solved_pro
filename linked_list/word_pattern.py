e4689bcb0c2ed43c6b7a38491259e1d9bd77d282
pivnati <natanmuleta911@gmail.com>
2025-02-03 23:01:27 -0700

    modify the code and optimize

--- linked_list/word_pattern.py
+++ linked_list/word_pattern.py
@@ -5,22 +5,21 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        words = s.split()  # Split the string into words
         
-        if len(pattern) != len(words):  # If lengths don't match, return False
+        if len(pattern) != len(words):  
             return False
         
         char_to_word = {}  # Dictionary to map pattern characters to words
-        word_to_char = {}  # Dictionary to map words to pattern characters
+        word_to_char = {}  
         
         for char, word in zip(pattern, words):  # Iterate through both together
             if char in char_to_word:  # Check if char is already mapped
-                if char_to_word[char] != word:  # If it maps to a different word, return False
+                if char_to_word[char] != word: 
                     return False
             else:
-                if word in word_to_char:  # If word is already mapped to another char, return False
+                if word in word_to_char: 
                     return False
                 char_to_word[char] = word
                 word_to_char[word] = char
         
-        return True  # If no mismatches found, return True
+        return True  
